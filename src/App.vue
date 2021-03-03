22350409f87d1832784ba5e4b7550ef1a51e8e5d
Sven Schnaible <Sven.Schnaible@gmail.com>
2021-03-03 12:32:43 +0100

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <md-app id="app">
+    <md-app-drawer md-permanent="full">
+      <SyndicatesDrawer />
+    </md-app-drawer>
+    <md-app-content>
+      <transition mode="out-in">
+        <router-view />
+      </transition>
+    </md-app-content>
+  </md-app>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import SyndicatesDrawer from './components/SyndicatesDrawer.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    SyndicatesDrawer
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
